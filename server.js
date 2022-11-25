b5e894464c9939a439e37ed3b3038e52178d35d9
Kayn Helga <32682968+kaynhelga9@users.noreply.github.com>
2022-11-25 14:23:54 +0700

    mongodb

--- server.js
+++ server.js
@@ -1,62 +1,65 @@
-const cors = require('cors')
-const express = require('express')
-const path = require('path')
-const { logEvents } = require('./middleware/log')
-const { logger } = require('./middleware/log')
-const errorHandler = require('./middleware/errorHandler')
-const corsOptions = require('././config/corsOption')
-const verifyJWT = require('./middleware/verifyJWT')
-const cookieParser = require('cookie-parser')
+require("dotenv").config();
+const cors = require("cors");
+const express = require("express");
+const path = require("path");
+const { logEvents } = require("./middleware/log");
+const { logger } = require("./middleware/log");
+const errorHandler = require("./middleware/errorHandler");
+const corsOptions = require("././config/corsOption");
+const verifyJWT = require("./middleware/verifyJWT");
+const cookieParser = require("cookie-parser");
+const mongoose = require("mongoose");
+const connectDB = require("./config/dbConn");
 
-const PORT = process.env.PORT | 8000
+const PORT = process.env.PORT | 8000;
 
-const app = express()
+connectDB();
+
+const app = express();
 
 // custom middleware logger
-app.use(logger)
+app.use(logger);
 
-
-app.use(cors(corsOptions))
+app.use(cors(corsOptions));
 
 // handle urlencoded data
-app.use(express.urlencoded({extended: false}))
+app.use(express.urlencoded({ extended: false }));
 
 // json
-app.use(express.json())
+app.use(express.json());
 
 // middleware for cookies
-app.use(cookieParser())
+app.use(cookieParser());
 
 // static
-app.use('/', express.static(path.join(__dirname, '/public')))
-
+app.use("/", express.static(path.join(__dirname, "/public")));
 
 // ROUTING
-app.use('/', require('./routes/root'))
-app.use('/register', require('./routes/register'))
-app.use('/auth', require('./routes/auth'))
-app.use('/refresh', require('./routes/refresh'))
-app.use('/logout', require('./routes/logout'))
+app.use("/", require("./routes/root"));
+app.use("/register", require("./routes/register"));
+app.use("/auth", require("./routes/auth"));
+app.use("/refresh", require("./routes/refresh"));
+app.use("/logout", require("./routes/logout"));
 
-app.use(verifyJWT) // only verify after this pathing
-app.use('/employees', require('./routes/api/employees'))
+app.use(verifyJWT); // only verify after this pathing
+app.use("/employees", require("./routes/api/employees"));
 
+app.all("/*", (req, res) => {
+  res.status(404);
+  if (req.accepts("html")) {
+    res.sendFile(path.join(__dirname, "views", "404.html"));
+  } else if (req.accepts("json")) {
+    res.json({ error: "404 Not Found" });
+  } else {
+    res.type("txt").send("404 Not Found");
+  }
+});
 
-app.all('/*', (req, res) => {
-    res.status(404)
-    if(req.accepts('html')) {
-        res.sendFile(path.join(__dirname, 'views', '404.html'))
-    } else if(req.accepts('json')) {
-        res.json({error: '404 Not Found'})
-    } else {
-        res.type('txt').send('404 Not Found')
-    }
+app.use(errorHandler);
 
-})
-
-app.use(errorHandler)
-
-
-app.listen(PORT, () => {
+mongoose.connection.once("open", () => {
+  console.log("connected to mongodb");
+  app.listen(PORT, () => {
     console.log(`Server running on ${PORT}`);
-})
+  });
+});
