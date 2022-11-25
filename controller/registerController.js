b83473490d7be73f104fd77e0d38e8771a2d9227
Kayn Helga <32682968+kaynhelga9@users.noreply.github.com>
2022-11-25 15:03:03 +0700

    mongoose data model

--- controller/registerController.js
+++ controller/registerController.js
@@ -1,42 +1,29 @@
-const usersDB = {
-    users: require('../model/users.json'),
-    setUsers: function (data) { this.users = data }
-}
-
-const fsPromises = require('fs/promises')
-const path = require('path')
-const bcrypt = require('bcrypt')
-
+const User = require("../model/User");
+const bcrypt = require("bcrypt");
 
 const handleNewUser = async (req, res) => {
-    const {user, pwd} = req.body
-    if (!user || !pwd) {
-        return res.status(400).json({ 'message': 'Missing username/password'})
-    }
-    // check for dupe users
-    const duplicate = usersDB.users.find(u => u.username === user)
-    if (duplicate) {
-        return res.sendStatus(409)
-    }
-    try {
-        // encrypt password
-        const hashed = await bcrypt.hash(pwd, 10)
+  const { user, pwd } = req.body;
+  if (!user || !pwd) {
+    return res.status(400).json({ message: "Missing username/password" });
+  }
+  // check for dupe users
+  const duplicate = await User.findOne({ username: user }).exec();
+  if (duplicate) return res.sendStatus(409);
+  try {
+    // encrypt password
+    const hashed = await bcrypt.hash(pwd, 10);
 
-        // store new user
-        const newUser = {
-            'username': user, 
-            'password': hashed,
-            'roles': {'User': 2001}
-        }
-        usersDB.setUsers([...usersDB.users, newUser])
-        await fsPromises.writeFile(path.join(__dirname, '..', 'model', 'users.json'), JSON.stringify(usersDB.users))
-        console.log(usersDB.users);
-        res.status(201).json({'success': `new user ${user} created`})
+    // create and store new user
+    const result = await User.create({
+      username: user,
+      password: hashed,
+    });
+    console.log(result);
 
-    } catch (error) {
-        res.status(500).json({ 'message': error.message})
-    }
-}
+    res.status(201).json({ success: `new user ${user} created` });
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+};
 
-
-module.exports = {handleNewUser}
+module.exports = { handleNewUser };
